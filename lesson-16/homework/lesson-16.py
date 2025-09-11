0d3e417088482c9b37d0d01dfb517b37ea819cb3
DataSniper2402 <m.rozmatov2402@gmail.com>
2025-09-11 16:46:59 +0500

    Create lesson-16.py

---
+++ lesson-16/homework/lesson-16.py
@@ -0,0 +1,71 @@
+# 1
+
+import numpy as np 
+my_list = [12.23, 13.32, 100, 36.32]
+
+np.array(my_list)
+
+# 2
+
+import numpy as np
+
+data1 = np.arange(2, 11).reshape(3, 3)
+
+
+# 3
+
+vector = np.zeros(10)
+vector[5] = 11
+
+# 4
+
+
+import numpy as np 
+data2 = np.arange(12, 38)
+ arr = np.array(data2)
+print(arr)
+
+# 5
+import numpy as np
+data = [(1, 2, 3, 4), astype(float)]
+type_data = data.astype(float)
+np.array(data)
+
+
+# 6
+
+import numpy as np
+
+celsius = np.array([0, 12, 45.21, 34, 99.91])
+fahrenheit = (celsius * 9/5) + 32
+celsius_back = (fahrenheit - 32) * 5/9
+print(celsius)
+print(fahrenheit)
+print(celsius_back)
+
+# 7
+
+import numpy as np
+app = np.array([10, 20, 30])
+app_data = np.append(app, np.arange(40, 100, 10))
+print(app_data)
+
+# 8
+
+import numpy as np
+
+arr = np.random.randint(0, 100, 10)
+print(arr)
+
+# 9
+
+import numpy as np
+arr = np.random.rand(10, 10)
+min_val = np.min(arr)
+max_val = np.max(arr)
+
+# 10
+
+import numpy as np
+arr = np.random.rand(3, 3, 3).
+
